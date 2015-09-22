7156331d9cd7d94e2731b08f849787031b1cd032
Glauber Ramos <glauberamos@gmail.com>
2015-09-22 14:53:38 -0300

    Changes on run logic

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ var root = 'http://jsonplaceholder.typicode.com';
 
 var console = {
 	log: function(data) {
-		$('#result').html(JSON.stringify(data));
+		$('#result').append(JSON.stringify(data));
+		$('#result').append('<br>');
 	}
 };
 
 $(function() {
 	$('#button').click(function() {
-		 eval($('textarea').val());
+		$('#result').html('');
+		try {
+			var result = eval($('textarea').val());
+			$('#result').append(result);
+
+			if(result === false) {
+				$('#result').append('false');				
+			}
+		} catch (e) {
+		    $('#result').append(e.message);
+		}
 	});
 });
